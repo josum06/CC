1662b81de4ee0a26d47d878209c948b2b484c549
Sumit Joshi <sumit.joshi0616@gmail.com>
2025-03-24 01:08:13 +0530

    Added Home Page

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -10,8 +10,10 @@ import Chats from "./pages/Chats";
 import { ClerkProvider } from "@clerk/clerk-react";
 import SignUpPage from "./pages/SignUp";
 import Login from "./pages/Login";
+import Home  from "./pages/Home";
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 
 function App() {
   return (
@@ -20,8 +22,8 @@ function App() {
         <Routes>
           <Route path="/Signup" element={<SignUpPage />} />
           <Route path="/Login" element={<Login />} />
-          <Route element={<AppLayout />}>
-            <Route path="/" />
+          <Route element={<AppLayout />} >
+            <Route path="/" element={<Home/>} />
             <Route path="/Notice" element={<Notice />} />
             <Route path="/Post" element={<Post />} />
             <Route path="/FacultyPost" element={<FacultyPost />} />
---
+++ myapp/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
+ function Home() {
+    const { user } =  useUser(); // Get Clerk user details
+
+      useEffect(() => {
+        if (user) {
+          fetch("http://localhost:3000/api/auth/save-user", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              clerkId: user.id,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              email: user.primaryEmailAddress?.emailAddress, 
+            }),
+          })
+            .then((res) => res.json())
+            .then((data) => console.log("User saved:", data))
+            .catch((err) => console.error("Error saving user:", err));
+        }
+      }, [user]);
+    return (
+        <div>
+            hello 
+        </div>
+    )
+}
+
+export default Home
